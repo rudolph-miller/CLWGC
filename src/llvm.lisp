eec099b96c330804981057716d5d3d6abd72fced
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-05-18 17:59:42 +0900

    upgrade llvm.lisp.

--- src/llvm.lisp
+++ src/llvm.lisp
@@ -1,7 +1,33 @@
 (in-package :cl-user)
 (defpackage clwgc.llvm
   (:use :cl
-        :clwgc.ast))
+        :clwgc.semantic)
+  (:export :*builder*
+           :*module*
+           :*ee*
+           :*fpm*
+           :*cons*
+           :with-module
+           :get-type
+           :append-block
+           :move-to
+           :add-function
+           :add-function-and-move-into
+           :args
+           :alloca
+           :ret
+           :constant
+           :store-var
+           :load-var
+           :init-var
+           :br
+           :cond-br
+           :call
+           :run-pass
+           :run
+           :make-cons
+           :get-car
+           :get-cdr))
 (in-package :clwgc.llvm)
 
 (defparameter *builder* nil)
@@ -12,27 +38,38 @@
 
 (defparameter *fpm* nil)
 
-(defparameter *integer* nil)
+(defparameter *cons* nil)
 
-(defparameter *void* nil)
+(defun get-type (key &optional arg)
+  (if (null key)
+      (llvm:void-type)
+      (ecase key
+        (:integer (llvm:int64-type))
+        (:bool (llvm:int1-type))
+        (:cons *cons*)
+        (:pointer (llvm:pointer-type arg)))))
+
+(defun declare-cons ()
+  (let ((ty (llvm:struct-create-named (llvm:global-context) "cons")))
+    (llvm:struct-set-body ty (list (get-type :integer) (get-type :integer)))
+    ty))
+
+(defmacro with-cons-declared (&body body)
+  `(let ((*cons* (declare-cons)))
+     ,@body))
 
 (defmacro with-module (&body body)
   `(llvm:with-objects ((*builder* llvm:builder)
                        (*module* llvm:module "CLWGC")
                        (*ee* llvm:execution-engine *module*)
                        (*fpm* llvm:function-pass-manager *module*))
-     (llvm:add-target-data (llvm:target-data *ee*) *fpm*)
-     (llvm:add-promote-memory-to-register-pass *fpm*)
-     (llvm:add-instruction-combining-pass *fpm*)
-     (llvm:add-reassociate-pass *fpm*)
-     (llvm:add-cfg-simplification-pass *fpm*)
-     ,@body))
-
-(defun get-type (key)
-  (if (null key)
-      (llvm:void-type)
-      (ecase key
-        (:integer (llvm:int64-type)))))
+     (with-cons-declared
+         (llvm:add-target-data (llvm:target-data *ee*) *fpm*)
+       (llvm:add-promote-memory-to-register-pass *fpm*)
+       (llvm:add-reassociate-pass *fpm*)
+       (llvm:add-gvn-pass *fpm*)
+       (llvm:initialize-function-pass-manager *fpm*)
+       ,@body)))
 
 (defun append-block (fn name)
   (llvm:append-basic-block fn name))
@@ -41,13 +78,26 @@
   (llvm:position-builder *builder* block))
 
 (defun add-function (name arg-t ret-t)
-  (let ((arr (make-array (length arg-t))))
+  (let ((arr (make-array (length arg-t)))
+        (ret (if (consp ret-t)
+                 (get-type (car ret-t) (get-type (cadr ret-t)))
+                 (get-type ret-t))))
     (when arg-t
       (loop for i from 0
             for item in arg-t
             do (setf (aref arr i) (get-type item))))
     (llvm:add-function *module* name
-                       (llvm:function-type (get-type ret-t) arr))))
+                       (llvm:function-type ret arr))))
+
+(defun add-function-and-move-into (name arg-t ret-t)
+  (let ((fn (add-function name arg-t ret-t)))
+    (move-to (append-block fn "entry"))
+    fn))
+
+(defun args (fn)
+  (when (stringp fn)
+    (setq fn (llvm:named-function *module* fn)))
+  (llvm:params fn))
 
 (defun alloca (type &optional (name "tmp"))
   (llvm:build-alloca *builder* (get-type type) name))
@@ -57,7 +107,8 @@
 
 (defun constant (type value)
   (ecase type
-    (:integer (llvm:const-int (get-type type) value))))
+    (:integer (llvm:const-int (get-type type) value))
+    (:bool (llvm:const-int (get-type type) value))))
 
 (defun store-var (var val)
   (llvm:build-store *builder* val var))
@@ -65,15 +116,52 @@
 (defun load-var (var &optional (name "tmp"))
   (llvm:build-load *builder* var name))
 
+(defun init-var (type &optional val (name "tmp"))
+  (let ((var (alloca type name)))
+    (when val
+      (store-var var val))
+    var))
+
 (defun br (dest)
   (llvm:build-br *builder* dest))
+
+(defun cond-br (if then else)
+  (let* ((comp (if (cffi:pointer-eq (llvm:type-of if) (get-type :bool))
+                   if
+                   (llvm:build-i-cmp *builder* :> if (constant :integer 0) "comp"))))
+    (llvm:build-cond-br *builder* comp then else)))
+
+(defun call (fn &optional args (name "tmp"))
+  (when (stringp fn)
+    (setq fn (llvm:named-function *module* fn)))
+  (llvm:build-call *builder* fn args name))
 
 (defun run-pass (fn)
   (when (stringp fn)
     (setq fn (llvm:named-function *module* fn)))
   (llvm:run-function-pass-manager *fpm* fn))
 
-(defun call (fn args &optional (name "tmp"))
+(defun run (fn &optional args)
   (when (stringp fn)
     (setq fn (llvm:named-function *module* fn)))
-  (llvm:build-call *builder* fn args name))
+  (llvm:generic-value-to-int (llvm:run-function *ee* fn args) nil))
+
+(defun make-cons (car cdr)
+  (let ((cons (alloca :cons)))
+    (setf (get-car cons) car)
+    (setf (get-cdr cons) cdr)
+    cons))
+
+(defun get-car (cons &optional (name "tmp"))
+  (llvm:build-struct-gep *builder* cons 0 name))
+
+(defun get-cdr (cons &optional (name "tmp"))
+  (llvm:build-struct-gep *builder* cons 1 name))
+
+(defun (setf get-car) (val cons)
+  (let ((car (get-car cons)))
+    (store-var car val)))
+
+(defun (setf get-cdr) (val cons)
+  (let ((cdr (get-cdr cons)))
+    (store-var cdr val)))
---
+++ t/llvm.lisp
@@ -0,0 +1,256 @@
+(in-package :cl-user)
+(defpackage clwgc-test.llvm
+  (:use :cl
+        :prove
+        :clwgc-test.init
+        :clwgc.llvm)
+  (:shadowing-import-from :clwgc.llvm
+                          :run))
+(in-package :clwgc-test.llvm)
+
+(plan nil)
+
+(defmacro is-ptr (target expect &optional commet)
+  `(is ,target
+       ,expect
+       ,@(when commet (list commet))
+       :test #'cffi:pointer-eq))
+
+(subtest "with-module"
+  (with-module
+    (ok *builder*
+        "*builder* is bound.")
+
+    (ok *module*
+        "*module* is bound.")
+
+    (ok *ee*
+        "*ee* is bound.")
+
+    (ok *fpm*
+        "*fpm* is bound.")
+
+    (ok *cons*
+        "*cons* is bound.")))
+
+
+(subtest "get-type"
+  (with-module
+    (is-ptr (get-type :integer)
+            (llvm:int64-type)
+            ":integer.")
+
+    (is-ptr (get-type :bool)
+            (llvm:int1-type)
+            ":bool.")
+
+    (is-ptr (get-type :cons)
+            *cons*
+            ":cons.")
+
+    (is-ptr (get-type :pointer (get-type :cons))
+            (llvm:pointer-type *cons*)
+            ":pointer.")))
+
+(subtest "append-block"
+  (with-module
+    (let ((main (add-function "main" nil nil)))
+      (append-block main "entry")
+      (is (llvm:count-basic-blocks main)
+          1
+          "con add block."))))
+
+(subtest "move-to"
+  (with-module
+    (skip 1 "can not get current block.")))
+
+(subtest "add-function"
+  (with-module
+    (let ((main (add-function "main" nil nil)))
+      (is-ptr (llvm:named-function *module* "main")
+              main
+              "can add a named function."))))
+
+(subtest "add-function-and-move-into"
+  (with-module
+    (skip 1 "can not get current block.")))
+
+(subtest "args"
+  (with-module
+    (let ((main (add-function "main" (list :integer) :integer)))
+      (is (length (args main))
+          1
+          "with function pointer.")
+
+      (is (length (args "main"))
+          1
+          "with string."))))
+
+(subtest "alloca"
+  (with-module
+    (add-function-and-move-into "main" nil nil)
+    (ok (not (cffi:null-pointer-p (alloca :integer)))
+        "can alloca.")))
+
+(subtest "ret"
+  (with-module
+    (let ((main (add-function-and-move-into "main" nil :integer)))
+      (ret (constant :integer 1))
+      (is (run main)
+          1
+          "can return value."))))
+
+(subtest "constant"
+  (with-module
+    (let ((main (add-function-and-move-into "main" nil :integer)))
+      (ret (constant :integer 1))
+      (is (run main)
+          1
+          ":integer."))
+
+    (let ((main (add-function-and-move-into "main" nil :integer)))
+      (ret (constant :bool 1))
+      (is (run main)
+          1
+          ":bool."))))
+
+(subtest "store-var, load-var"
+  (with-module
+    (let ((main (add-function-and-move-into "main" nil :integer))
+          (i (alloca :integer)))
+      (store-var i (constant :integer 1))
+      (ret (load-var i))
+      (is (run main)
+          1
+          "can strore and load."))))
+
+(subtest "init-var"
+  (with-module
+    (let ((main (add-function-and-move-into "main" nil :integer))
+          (i (init-var :integer (constant :integer 1))))
+      (ret (load-var i))
+      (is (run main)
+          1
+          "can alloca and store."))))
+
+(subtest "br"
+  (with-module
+    (let* ((main (add-function-and-move-into "main" nil :integer))
+           (end (append-block main "end")))
+      (br end)
+      (move-to end)
+      (ret (constant :integer 1))
+      (is (run main)
+          1
+          "can br."))))
+
+(subtest "cond-br"
+  (with-module
+    (let* ((main (add-function-and-move-into "main" (list :bool) :integer))
+           (then (append-block main "then"))
+           (else (append-block main "else")))
+      (cond-br (car (args main)) then else)
+      (move-to then)
+      (ret (constant :integer 1))
+      (move-to else)
+      (ret (constant :integer 2))
+      (let ((sub (add-function-and-move-into "sub" nil :integer)))
+         (ret (call main (list (constant :bool 1))))
+        (is (run sub)
+            1
+            "then with 1 in int1."))
+
+      (let ((sub (add-function-and-move-into "sub" nil :integer)))
+         (ret (call main (list (constant :bool 0))))
+        (is (run sub)
+            2
+            "then with 0 in int1.")))
+
+    (let* ((main (add-function-and-move-into "main" (list :integer) :integer))
+           (then (append-block main "then"))
+           (else (append-block main "else")))
+      (cond-br (car (args main)) then else)
+      (move-to then)
+      (ret (constant :integer 1))
+      (move-to else)
+      (ret (constant :integer 2))
+      (let ((sub (add-function-and-move-into "sub" nil :integer)))
+         (ret (call main (list (constant :integer 1))))
+        (is (run sub)
+            1
+            "then with 1 in int64."))
+
+      (let ((sub (add-function-and-move-into "sub" nil :integer)))
+         (ret (call main (list (constant :integer 0))))
+        (is (run sub)
+            2
+            "then with 0 in int64.")))))
+
+(subtest "call"
+  (with-module
+    (let ((main (add-function-and-move-into "main" (list :integer) :integer)))
+      (ret (car (args main)))
+      (let ((sub (add-function-and-move-into "sub" nil :integer)))
+        (ret (call main (list (constant :integer 1))))
+        (is (run sub)
+            1
+            "can call function.")))))
+
+(subtest "run-pass"
+  (with-module
+    (let ((main (add-function-and-move-into "main" nil :integer)))
+      (ret (llvm:build-add *builder* (constant :integer 1) (constant :integer 2) "tmp"))
+      (run-pass main)
+      (ok t "do not raise errors."))))
+
+(subtest "run"
+  (with-module
+    (let ((main (add-function-and-move-into "main" nil :integer)))
+      (ret (constant :integer 1))
+      (is (run main)
+          1
+          "can run function."))))
+
+(subtest "make-cons"
+  (with-module
+    (add-function-and-move-into "main" nil :integer)
+
+      (is-ptr (llvm:type-of (load-var (make-cons (constant :integer 1) (constant :integer 2))))
+              (get-type :cons)
+              "can make-cons.")))
+
+(subtest "get-car"
+  (with-module
+    (let ((main (add-function-and-move-into "main" nil :integer))
+          (i (make-cons (constant :integer 1) (constant :integer 2))))
+      (ret (load-var (get-car i)))
+      (is (run main)
+          1
+          "can get."))
+
+    (let ((main (add-function-and-move-into "main" nil :integer))
+          (i (make-cons (constant :integer 1) (constant :integer 2))))
+      (setf (get-car i) (constant :integer 2))
+      (ret (load-var (get-car i)))
+      (is (run main)
+          2
+          "can setf."))))
+
+(subtest "get-cdr"
+  (with-module
+    (let ((main (add-function-and-move-into "main" nil :integer))
+          (i (make-cons (constant :integer 1) (constant :integer 2))))
+      (ret (load-var (get-cdr i)))
+      (is (run main)
+          2
+          "can get."))
+
+    (let ((main (add-function-and-move-into "main" nil :integer))
+          (i (make-cons (constant :integer 1) (constant :integer 2))))
+      (setf (get-cdr i) (constant :integer 1))
+      (ret (load-var (get-cdr i)))
+      (is (run main)
+          1
+          "can setf."))))
+
+(finalize)
